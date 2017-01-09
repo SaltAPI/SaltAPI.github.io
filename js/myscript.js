26de690f4e0867f58381f39b0ea3d97cf29e1956
FernandVEYRIER <fernand.veyrier@epitech.eu>
2017-01-09 12:13:59 +0100

    added scrollbar and scrollnav

---
+++ js/myscript.js
@@ -0,0 +1,36 @@
+/**
+ * Created by veyrie_f on 09/01/17.
+ */
+
+$('nav a').on('click', function() {
+    console.log($(this).attr('data-scroll'));
+
+    var scrollAnchor = $(this).attr('data-scroll'),
+        scrollPoint = $('section[data-anchor="' + scrollAnchor + '"]').offset().top - 100;
+
+    $('body,html').animate({
+        scrollTop: scrollPoint
+    }, 500);
+
+    return false;
+
+});
+
+$(window).scroll(function() {
+    var windscroll = $(window).scrollTop();
+    if (windscroll >= 100) {
+        $('nav').addClass('fixed');
+        $('.wrapper section').each(function (i) {
+            if ($(this).position().top <= windscroll + 100) {
+                $('nav.subnav ul li.active').removeClass('active');
+                $('nav.subnav ul li').eq(i).addClass('active');
+            }
+        });
+
+    } else {
+
+        $('nav').removeClass('fixed');
+        $('nav.subnav ul li.active').removeClass('active');
+        $('nav.subnav ul li:first').addClass('active');
+    }
+}).scroll();
