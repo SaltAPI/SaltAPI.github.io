74e3b01ffc81bc58a81caa2d2ca25e9a3afa9c20
FernandVEYRIER <fernand.veyrier@epitech.eu>
2017-01-10 16:54:38 +0100

    Added animations

--- js/myscript.js
+++ js/myscript.js
@@ -23,6 +23,7 @@ $('a[data-scroll]').on('click', function() {
     return false;
 });
 
+/** navbar update **/
 $(window).scroll(function() {
     var windscroll = $(window).scrollTop();
     if (windscroll >= 100) {
@@ -48,4 +49,29 @@ $(function() {
         $('.imagepreview').attr('src', $(this).find('img').attr('src'));
         $('#imagemodal').modal('show');
     });
+});
+
+/** fade in anim **/
+$(document).ready(function() {
+
+    /* Every time the window is scrolled ... */
+    $(window).scroll( function(){
+
+        /* Check the location of each desired element */
+        $('.fade-in').each( function(i){
+
+            var bottom_of_object = $(this).position().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+            /* If the object is completely visible in the window, fade it in */
+            if( bottom_of_window > bottom_of_object ){
+                $(this).animate({'opacity': '1'}, 0);
+                $(this).css('transform', 'translateX(0px)');
+                $(this).css('-webkit-transform', 'translateX(0px)');
+            }
+
+        });
+
+    });
+
 });
